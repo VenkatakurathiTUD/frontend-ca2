02eb31121101d360d2859428336f901c944c6621
mnraomq <mnraomq@gmail.com>
2025-04-17 17:16:12 +0100

    changes to files

--- fe-server.js
+++ fe-server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 const http = require('http'); // Import http at the top
 
 // Middleware to parse request body for POST requests
