51f7c479467c87cfc6c7ceb46c1602fc5f517cde
mnraomq <mnraomq@gmail.com>
2025-04-19 16:51:24 +0100

    added recipes and app files at frontend application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recipes from './Recipes';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Recipe App</h1>
+      <Recipes />
+    </div>
+  );
+};
+
+export default App;
